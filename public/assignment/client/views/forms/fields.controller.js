25830bb03d55906c2b1bb58379ac75427e8af8a9
Brittany Chiang <brittany.chiang@gmail.com>
2016-03-18 21:18:19 -0400

    server side development

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -5,115 +5,151 @@
 
     function FieldsController(FieldService, $scope, $routeParams) {
         var vm = this;
-        var userId = $routeParams.userId;
-        var formId = $routeParams.formId;
-        vm.fields = [];
+        vm.cField = null;
+        vm.eField = null;
+        vm.editField = editField;
+        vm.commitEdit = commitEdit;
+        vm.deleteField = deleteField;
+        vm.addField = addField;
+        vm.reorder = reorder;
+        vm.options = [
+            'Single Line Text Field',
+            'Multi Line Text Field',
+            'Date Field',
+            'Dropdown Field',
+            'Checkboxes Field',
+            'Radio Buttons Field'
+        ];
+        vm.selection = vm.options[0];
+        vm.fieldOptions = null;
+        var formId = "000";
+        if ($routeParams.formId) {
+            formId = $routeParams.formId;
+        }
+
+        var optionMap = [{
+            key: "Single Line Text Field",
+            value: "TEXT"
+        }, {
+            key: "Multi Line Text Field",
+            value: "TEXTAREA"
+        }, {
+            key: "Date Field",
+            value: "DATE"
+        }, {
+            key: "Dropdown Field",
+            value: "OPTIONS"
+        }, {
+            key: "Checkboxes Field",
+            value: "CHECKBOXES"
+        }, {
+            key: "Radio Buttons Field",
+            value: "RADIOS"
+        }];
+
+        function render(response) {
+            vm.display = response.data;
+            vm.fields = response.data;
+        }
 
         function init() {
             FieldService
-                .getFieldsForForm(formId)
+                .findFieldsByForm(formId)
+                .then(render);
+            FormService
+                .getFormById(formId)
                 .then(function(response) {
-                    if (response.data) {
-                        vm.fields = response.data;
-                    }
-                });
-
+                    vm.form = response.data;
+                })
         }
         init();
 
-        vm.addField = addField;
-        $vm.deleeUser = deleteField;
-
-        function addField(fieldType) {
-            var field = null;
-            //Set default field information
-            if (fieldType === "Text") {
-                field = {
-                    "_id": null,
-                    "label": "New Text Field",
-                    "type": "Text",
-                    "placeholder": "New Field"
-                };
-            } else if (fieldType === "Textarea") {
-                field = {
-                    "_id": null,
-                    "label": "New Text Field",
-                    "type": "Textarea",
-                    "placeholder": "New Field"
-                };
-            } else if (fieldType === "Date") {
-                field = {
-                    "_id": null,
-                    "label": "New Date Field",
-                    "type": "Date"
-                };
-            } else if (fieldType === "Options") {
-                field = {
-                    "_id": null,
-                    "label": "New Dropdown",
-                    "type": "Options",
-                    "options": [{
-                        "label": "Option 1",
-                        "value": "OPTION_1"
-                    }, {
-                        "label": "Option 2",
-                        "value": "OPTION_2"
-                    }, {
-                        "label": "Option 3",
-                        "value": "OPTION_3"
-                    }]
-                };
-            } else if (fieldType === "Checkboxes") {
-                field = {
-                    "_id": null,
-                    "label": "New Checkboxes",
-                    "type": "CHECKBOXES",
-                    "options": [{
-                        "label": "Option A",
-                        "value": "OPTION_A"
-                    }, {
-                        "label": "Option B",
-                        "value": "OPTION_B"
-                    }, {
-                        "label": "Option C",
-                        "value": "OPTION_C"
-                    }]
-                };
-            } else {
-                field = {
-                    "_id": null,
-                    "label": "New Radio Buttons",
-                    "type": "RADIOS",
-                    "options": [{
-                        "label": "Option X",
-                        "value": "OPTION_X"
-                    }, {
-                        "label": "Option Y",
-                        "value": "OPTION_Y"
-                    }, {
-                        "label": "Option Z",
-                        "value": "OPTION_Z"
-                    }]
-                };
-            }
-
+        function sendEdit(field) {
+            vm.cField = null;
             FieldService
-                .createFieldForForm(formId, field)
-                .then(function(response) {
-                    if (response.data) {
-                        vm.fields = response.data;
-                    }
-                });
+                .updateField(formId, field._id, field)
+                .then(init);
+        }
+
+        function reorder() {
+            vm.form.fields = vm.fields;
+            FormService
+                .updateFormById(formId, vm.form)
+                .then(init);
+
         }
 
         function deleteField(field) {
+            vm.cField = null;
             FieldService
-                .deleteFieldFromForm(formId, field._id)
-                .then(function(response) {
-                    if (response.data) {
-                        vm.fields = response.data;
-                    }
-                });
+                .deleteField(formId, field._id)
+                .then(init);
+        }
+
+        function translateFieldType(fieldType) {
+            for (var k in optionMap) {
+                console.log(optionMap[k].key + " " + optionMap[k].value);
+                if (optionMap[k].key == fieldType) {
+                    return optionMap[k].value;
+                }
+            }
+        }
+
+        function addField(fieldType) {
+            var field = {
+                "label": "",
+                "type": translateFieldType(fieldType),
+                "placeholder": "",
+                "options": null
+            };
+            console.log(field);
+            FieldService
+                .createField(formId, field)
+                .then(init);
+        }
+
+
+        function editField(field) {
+            vm.eField = field;
+
+            var isOption = !(vm.eField.type === 'TEXT' || vm.eField.type === 'TEXTAREA');
+
+            if (isOption) {
+                var optionList = [];
+                var ol = vm.eField.options;
+                for (var o in ol) {
+                    optionList.push(ol[o].label + ":" + ol[o].value)
+                }
+                console.log(optionList);
+                vm.optionText = optionList.join("\n");
+                console.log(vm.optionText);
+            }
+        }
+
+        function commitEdit(field) {
+            vm.eField = field;
+
+            var isOption = !(field.type == 'TEXT' || field.type == 'TEXTAREA');
+
+            var optionArray = [];
+            if (isOption) {
+                console.log(vm.optionText);
+                var oa = vm.optionText;
+                for (var o in oa) {
+                    var a = oa[o].split(":");
+                    optionArray.push({
+                        label: a[0],
+                        value: a[1]
+                    });
+                }
+                vm.eField.options = optionArray;
+
+            } else {}
+            console.log(vm.eField._id);
+            FieldService
+                .updateField(formId, vm.eField._id, vm.eField)
+                .then(init);
+            vm.eField = null;
         }
     }
 })();

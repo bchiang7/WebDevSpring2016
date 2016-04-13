e082e980c04196e33a53b2e5489a95d40ab55755
Brittany Chiang <brittany.chiang@gmail.com>
2016-04-12 21:55:40 -0400

    WHY DOESN'T SORTABLE STICK

--- public/assignment/server/models/user.schema.server.js
+++ public/assignment/server/models/user.schema.server.js
@@ -3,15 +3,14 @@ module.exports = function() {
     var mongoose = require("mongoose");
     var FormSchema = require("./form.schema.server.js")(mongoose);
 
-    // use mongoose to declare a user schema
     var UserSchema = mongoose.Schema({
-        username: String,   // alice
-        password: String,   // p@ssw0rd
-        firstName: String,  // Alice
-        lastName: String,   // Wonderland
-        emails: [String],   // ['alice@wonderland.com', 'alice@gmail.com']
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        emails: [String],
         roles: [String],
-        phones: [String]    // ['123-234-4321', '234-432-2344']
+        phones: [String]
     }, {collection: 'assignment.user'});
 
     return UserSchema;
